34756aa3138918d96a1b656d9d2f5e879274d7a0
Aldo Jean <aldojeans@gmail.com>
2019-12-02 00:39:13 -0300

    DDL para a criação da base de dados cooperforte-clientes

---
+++ ddl-clientes-cooperforte.sql
@@ -0,0 +1,21 @@
+﻿CREATE TABLE clientes.usuario
+(
+  id bigserial NOT NULL PRIMARY KEY,
+  nome character varying(150),
+  email character varying(100),
+  senha character varying(20),
+  data_cadastro date DEFAULT NOW()
+);
+
+CREATE TABLE clientes.cliente
+(
+  id bigserial NOT NULL PRIMARY KEY ,
+  nome character varying(100) NOT NULL,
+  mes integer NOT NULL,
+  ano integer NOT NULL,
+  valor numeric(16,2),
+  tipo character varying(20) CHECK ( tipo in ('PUBLICO', 'PRIVADO') ) NOT NULL,
+  status character varying(20) CHECK ( status IN ('CADASTRAMENTO', 'CANCELADO', 'EFETIVADO' ) ) NOT NULL,
+  id_usuario bigint REFERENCES clientes.usuario (id) NOT NULL,
+  data_cadastro date DEFAULT NOW()
+);
